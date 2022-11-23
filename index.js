c33e9660e8075523c0aa4d41a455386b204b4ea9
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-11-23 22:22:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(trip){
+  let rideDistance = trip; 
+  if (rideDistance <= 400){
+    rideDistance = 'This one is on me!';
+    return rideDistance
+  } else if (rideDistance > 2000 && rideDistance < 2500) {
+    rideDistance = 'I will gladly take your thirty bucks.';
+    return rideDistance
+  } else {
+    rideDistance = 'No can do.';
+    return rideDistance
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(goodcity){
+  let city = goodcity
+  if (city === 'NYC') {
+    city = 'Ok, sounds good.';
+    return city
+  } else {
+    city = 'No go.';
+    return city
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(charm){
+  let tip = charm;
+  if (tip === 'generous') {
+    tip = 'Thank you so much.';
+    return tip
+  } else if (tip === 'not as generous') {
+    tip = 'Thank you.';
+    return tip
+  } else {
+    tip = 'Bye.';
+    return tip
+  }
 }
